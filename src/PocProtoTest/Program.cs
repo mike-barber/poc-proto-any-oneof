2656ba6f656d447128de34135c819bec23ed332c
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-04-29 12:58:31 +0100

    Add project files.

---
+++ src/PocProtoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PocProtoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
